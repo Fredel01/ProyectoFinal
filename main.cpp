9e127be818a9d7450b6aa6f784345bc9780f58f0
Ariel Orellana <lpenao@miumg.edu.gt>
2025-05-14 20:18:02 -0600

    prueba de comunicación

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+using namespace std;
+
+main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+// This is a simple C++ program that prints "Hello, World!" to the console.
