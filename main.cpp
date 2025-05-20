e053ceb3c779103434f8731e076d1107058fb744
Fredy Moran <elianmoran2019@gmail.com>
2025-05-20 00:11:09 -0600

    Corrección de errores
    Corregimos el grave error en las opciones actualizar y eliminar de todas las tablas

--- main.cpp
+++ main.cpp
@@ -15,33 +15,35 @@ public:
         : conn(conexion), orm(conexion, "ESTUDIANTES", {"ID_ESTUDIANTE", "NOMBRE", "APELLIDO", "CORREO", "FECHA_NACIMIENTO"}) {}
 
     void crear() {
+        EloquentORM alumno(conn, "estudiantes", {"id", "NOMBRE", "APELLIDO", "CORREO", "FECHA_NACIMIENTO"});
+        int idEstudiante;
         string nombre, apellido, correo, fecha;
-        int id_estudiante;
-        cout << "ID Estudiante: "; cin >> id_estudiante;
+        cout << "ID Estudiante: "; cin >> idEstudiante;
         cout << "Nombre: "; cin >> nombre;
         cout << "Apellido: "; cin >> apellido;
         cout << "Correo: "; cin >> correo;
         cout << "Fecha de nacimiento (YYYY-MM-DD): "; cin >> fecha;
 
-        orm.set("ID_ESTUDIANTE", to_string(id_estudiante));
-        orm.set("NOMBRE", nombre);
-        orm.set("APELLIDO", apellido);
-        orm.set("CORREO", correo);
-        orm.set("FECHA_NACIMIENTO", fecha);
+        alumno.set("id", to_string(idEstudiante));
+        alumno.set("NOMBRE", nombre);
+        alumno.set("APELLIDO", apellido);
+        alumno.set("CORREO", correo);
+        alumno.set("FECHA_NACIMIENTO", fecha);
 
-        cout << (orm.create() ? "Estudiante creado.\n" : "Error al crear estudiante.\n");
+        cout << (alumno.create() ? "Alumno creado.\n" : "Error al crear el alumno.\n");
     }
 
     void leer() {
         auto lista = orm.getAll();
         for (auto& reg : lista) {
-            cout << "ID: " << reg["ID_ESTUDIANTE"] << ", Nombre: " << reg["NOMBRE"] 
+            cout << "ID: " << reg["id"] << ", Nombre: " << reg["NOMBRE"] 
                  << ", Apellido: " << reg["APELLIDO"] << ", Correo: " << reg["CORREO"]
                  << ", Fecha: " << reg["FECHA_NACIMIENTO"] << endl;
         }
     }
 
     void actualizar() {
+        EloquentORM orm(conn, "estudiantes", {"id", "NOMBRE", "APELLIDO", "CORREO", "FECHA_NACIMIENTO"});
         int id;
         cout << "ID del estudiante: "; cin >> id;
         string nombre, apellido, correo, fecha;
@@ -50,7 +52,7 @@ public:
         cout << "Nuevo Correo: "; cin >> correo;
         cout << "Nueva Fecha (YYYY-MM-DD): "; cin >> fecha;
 
-        orm.set("ID_ESTUDIANTE", to_string(id));
+        orm.set("id", to_string(id));
         orm.set("NOMBRE", nombre);
         orm.set("APELLIDO", apellido);
         orm.set("CORREO", correo);
@@ -60,11 +62,13 @@ public:
     }
 
     void eliminar() {
+        EloquentORM alumno(conn,"estudiantes",{"NOMBRE", "APELLIDO", "CORREO","FECHA_NACIMIENTO"});
         int id;
-        cout << "ID del estudiante: "; cin >> id;
-        orm.set("ID_ESTUDIANTE", to_string(id));
-        orm.find(id);
-        cout << (orm.remove() ? "Estudiante eliminado.\n" : "Error al eliminar estudiante.\n");
+        cout << "Ingrese el id de estudiante que desea eliminar: \n"; 
+        cin >> id;
+        alumno.find(id);
+        cout << "Se elimino al alumno: " << alumno.get("NOMBRE") << endl;
+        alumno.remove(); 
     }
 };  
 
@@ -76,46 +80,56 @@ public:
         : conn(conexion), orm(conexion, "DOCENTES", {"ID_DOCENTE", "NOMBRE", "ESPECIALIDAD"}) {}
 
     void crear() {
+        EloquentORM profesor(conn, "DOCENTES", {"id", "NOMBRE", "APELLIDO", "ESPECIALIDAD"});
         int id_docente;
-        string nombre, especialidad;
+        string nombre, apellido, especialidad;
         cout << "ID Docente: "; cin >> id_docente;
-        cout << "Nombre: "; cin >> nombre;
+        cout << "Nombre: "; cin.ignore(); getline(cin, nombre);
+        cout << "Apellido: "; cin.ignore(); getline(cin, apellido);
         cout << "Especialidad: "; cin >> especialidad;
 
-        orm.set("ID_DOCENTE", to_string(id_docente));
-        orm.set("NOMBRE", nombre);
-        orm.set("ESPECIALIDAD", especialidad);
+        profesor.set("id", to_string(id_docente));
+        profesor.set("NOMBRE", nombre);
+        profesor.set("APELLIDO", apellido);
+        profesor.set("ESPECIALIDAD", especialidad);
 
-        cout << (orm.create() ? "Docente creado.\n" : "Error al crear docente.\n");
+        cout << (profesor.create() ? "Docente creado.\n" : "Error al crear docente.\n");
     }
 
     void leer() {
         auto lista = orm.getAll();
         for (auto& reg : lista) {
-            cout << "ID: " << reg["ID_DOCENTE"] << ", Nombre: " << reg["NOMBRE"] 
+            cout << "ID: " << reg["id"] << ", Nombre: " << reg["NOMBRE"] 
+                 << ", Apellido: " << reg["APELLIDO"]
                  << ", Especialidad: " << reg["ESPECIALIDAD"] << endl;
         }
     }
 
     void actualizar() {
+        EloquentORM orm(conn, "docentes", {"id", "NOMBRE", "APELLIDO", "ESPECIALIDAD"});
         int id;
         cout << "ID del docente: "; cin >> id;
-        string nombre, especialidad;
+        string nombre, apellido, especialidad;
         cout << "Nuevo Nombre: "; cin >> nombre;
-        cout << "Nueva Especialidad: "; cin >> especialidad;
+        cout << "Nuevo Apellido: "; cin >> apellido;
+        cout << "Nueva Especialidad: "; cin.ignore(); getline(cin, especialidad);
 
-        orm.set("ID_DOCENTE", to_string(id));
+        orm.set("id", to_string(id));
         orm.set("NOMBRE", nombre);
+        orm.set("APELLIDO", apellido);
         orm.set("ESPECIALIDAD", especialidad);
 
         cout << (orm.update() ? "Docente actualizado.\n" : "Error al actualizar docente.\n");
     }
 
     void eliminar() {
+        EloquentORM profesor(conn,"docentes",{"NOMBRE", "APELLIDO", "ESPECIALIDAD"});
         int id;
-        cout << "ID del docente: "; cin >> id;
-        orm.set("ID_DOCENTE", to_string(id));
-        cout << (orm.remove() ? "Docente eliminado.\n" : "Error al eliminar docente.\n");
+        cout << "Ingrese el id de docente que desea eliminar: \n"; 
+        cin >> id;
+        profesor.find(id);
+        cout << "Se elimino al docente: " << profesor.get("NOMBRE") << endl;
+        profesor.remove(); 
     }
 };
 
@@ -127,41 +141,46 @@ public:
         : conn(conexion), orm(conexion, "CURSOS", {"ID_CURSO", "NOMBRE", "CREDITOS", "ID_DOCENTE"}) {}
 
     void crear() {
-        string nombre;
+        EloquentORM materia(conn, "cursos", {"id", "NOMBRE_CURSO", "CREDITOS", "DESCRIPCION", "ID_DOCENTE"});
+        string nombre, descripcion;
         int creditos, id_docente, id_curso;
         cout << "ID Curso: "; cin >> id_curso;
-        cout << "Nombre del curso: "; cin >> nombre;
+        cout << "Nombre del curso: "; cin.ignore(); getline(cin, nombre);
         cout << "Créditos: "; cin >> creditos;
+        cout << "Descripcion: "; cin.ignore(); getline(cin, descripcion);
         cout << "ID del docente: "; cin >> id_docente;
 
-        orm.set("ID_CURSO", to_string(id_curso));
-        orm.set("NOMBRE", nombre);
-        orm.set("CREDITOS", to_string(creditos));
-        orm.set("ID_DOCENTE", to_string(id_docente));
+        materia.set("id", to_string(id_curso));
+        materia.set("NOMBRE_CURSO", nombre);
+        materia.set("CREDITOS", to_string(creditos));
+        materia.set("DESCRIPCION", descripcion);
+        materia.set("ID_DOCENTE", to_string(id_docente));
 
-        cout << (orm.create() ? "Curso creado.\n" : "Error al crear curso.\n");
+        cout << (materia.create() ? "Curso creado.\n" : "Error al crear curso.\n");
     }
 
     void leer() {
         auto lista = orm.getAll();
         for (auto& reg : lista) {
-            cout << "ID: " << reg["ID_CURSO"] << ", Nombre: " << reg["NOMBRE"]
+            cout << "ID: " << reg["id"] << ", Nombre: " << reg["NOMBRE_CURSO"]
                  << ", Créditos: " << reg["CREDITOS"]
+                 << ", Descripción: " << reg["DESCRIPCION"]
                  << ", ID Docente: " << reg["ID_DOCENTE"] << endl;
         }
     }
 
     void actualizar() {
+        EloquentORM orm(conn, "cursos", {"id", "NOMBRE_CURSO", "CREDITOS", "DESCRIPCION", "ID_DOCENTE"});
         int id;
         cout << "ID del curso: "; cin >> id;
         string nombre;
         int creditos, id_docente;
-        cout << "Nuevo Nombre: "; cin >> nombre;
+        cout << "Nuevo Nombre: "; cin.ignore(); getline(cin, nombre);
         cout << "Créditos: "; cin >> creditos;
         cout << "ID Docente: "; cin >> id_docente;
 
-        orm.set("ID_CURSO", to_string(id));
-        orm.set("NOMBRE", nombre);
+        orm.set("id", to_string(id));
+        orm.set("NOMBRE_CURSO", nombre);
         orm.set("CREDITOS", to_string(creditos));
         orm.set("ID_DOCENTE", to_string(id_docente));
 
@@ -169,10 +188,13 @@ public:
     }
 
     void eliminar() {
+        EloquentORM materia(conn,"cursos",{"NOMBRE_CURSO", "CREDITOS", "DESCRIPCION", "ID_DOCENTE"});
         int id;
-        cout << "ID del curso: "; cin >> id;
-        orm.set("ID_CURSO", to_string(id));
-        cout << (orm.remove() ? "Curso eliminado.\n" : "Error al eliminar curso.\n");
+        cout << "Ingrese el id de curso que desea eliminar: \n"; 
+        cin >> id;
+        materia.find(id);
+        cout << "Se elimino el curso: " << materia.get("NOMBRE_CURSO") << endl;
+        materia.remove();
     }
 };
 
@@ -184,17 +206,23 @@ public:
         : conn(conexion), orm(conexion, "NOTAS", {"ID_NOTA", "ID_ESTUDIANTE", "ID_CURSO", "CALIFICACION"}) {}
 
     void crear() {
+        EloquentORM orm(conn, "NOTAS", {"id", "ID_ESTUDIANTE", "ID_CURSO", "TIPO", "NOTA", "FECHA_REGISTRO"});
         int id_estudiante, id_curso, id_nota;
         double calificacion;
+        string fecha, tipo;
         cout << "ID Nota: "; cin >> id_nota;
         cout << "ID Estudiante: "; cin >> id_estudiante;
         cout << "ID Curso: "; cin >> id_curso;
+        cout << "Tipo: "; cin >> tipo;
         cout << "Calificación: "; cin >> calificacion;
+        cout << "Fecha de registro (YYYY-MM-DD): "; cin >> fecha;
 
-        orm.set("ID_NOTA", to_string(id_nota));
+        orm.set("id", to_string(id_nota));
         orm.set("ID_ESTUDIANTE", to_string(id_estudiante));
         orm.set("ID_CURSO", to_string(id_curso));
-        orm.set("CALIFICACION", to_string(calificacion));
+        orm.set("NOTA", to_string(calificacion));
+        orm.set("TIPO", tipo);
+        orm.set("FECHA_REGISTRO", fecha);
 
         cout << (orm.create() ? "Nota registrada.\n" : "Error al registrar nota.\n");
     }
@@ -202,33 +230,42 @@ public:
     void leer() {
         auto lista = orm.getAll();
         for (auto& reg : lista) {
-            cout << "ID: " << reg["ID_NOTA"] << ", Estudiante: " << reg["ID_ESTUDIANTE"]
+            cout << "ID: " << reg["id"] << ", Estudiante: " << reg["ID_ESTUDIANTE"]
                  << ", Curso: " << reg["ID_CURSO"]
-                 << ", Calificación: " << reg["CALIFICACION"] << endl;
+                 << ", Calificación: " << reg["NOTA"] << endl
+                 << "Tipo: " << reg["TIPO"] << ", Fecha: " << reg["FECHA_REGISTRO"] << endl;
         }
     }
 
     void actualizar() {
+        EloquentORM orm(conn, "NOTAS", {"id", "ID_ESTUDIANTE", "ID_CURSO", "TIPO", "NOTA", "FECHA_REGISTRO"});    
         int id;
         cout << "ID de la nota: "; cin >> id;
         int id_estudiante, id_curso;
         double calificacion;
+        string fecha, tipo;
         cout << "Nuevo ID Estudiante: "; cin >> id_estudiante;
         cout << "Nuevo ID Curso: "; cin >> id_curso;
+        cout << "Nuevo Tipo: "; cin.ignore(); getline(cin, tipo);
         cout << "Nueva Calificación: "; cin >> calificacion;
+        cout << "Nueva Fecha (YYYY-MM-DD): "; cin >> fecha;
 
-        orm.set("ID_NOTA", to_string(id));
+        orm.set("id", to_string(id));
         orm.set("ID_ESTUDIANTE", to_string(id_estudiante));
         orm.set("ID_CURSO", to_string(id_curso));
-        orm.set("CALIFICACION", to_string(calificacion));
+        orm.set("TIPO", tipo);
+        orm.set("NOTA", to_string(calificacion));
+        orm.set("FECHA_REGISTRO", fecha);
 
         cout << (orm.update() ? "Nota actualizada.\n" : "Error al actualizar nota.\n");
     }
 
     void eliminar() {
+        EloquentORM orm(conn, "NOTAS", {"id", "ID_ESTUDIANTE", "ID_CURSO", "TIPO", "NOTA", "FECHA_REGISTRO"});
         int id;
         cout << "ID de la nota: "; cin >> id;
-        orm.set("ID_NOTA", to_string(id));
+        orm.find(id);
+        cout << "Se elimino la nota: " << orm.get("TIPO") << endl;
         cout << (orm.remove() ? "Nota eliminada.\n" : "Error al eliminar nota.\n");
     }
 };
